82d63130da04fd5b9b1c36f83cec788479bddee3
Teddy Nguyen <ducnh251092@gmail.com>
2023-11-14 09:47:35 +0700

    impl

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import ganache from 'ganache';
+
+const server = ganache.server();
+const PORT = +(process.env.PORT || 8545);
+server.listen(PORT, '127.0.0.1', async err => {
+  if (err) throw err;
+
+  console.log(`Ganache listening on port ${server.address().port}...`);
+  const provider = server.provider;
+  const accounts = await provider.request({
+    method: 'eth_accounts',
+    params: [],
+  });
+  console.log('accounts:', accounts);
+});
